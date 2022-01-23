57dd2eca3991f4f9b8a48022da8805869383c263
Duan Pham <duanziggs@gmail.com>
2022-01-23 23:10:20 +0700

    change pakage.json file

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(_dirname + './dist'));
+app.use(express.static(_dirname + './dist/delete-project'));
 
 app.get('/*', (req, res) =>
-    res.sendFile(path.join(_dirname + '/dist/index.html')),
+    res.sendFile(path.join(_dirname,'dist','delete-project','index.html')),
 );
 
 // Start the app by listening on the default Heroku port
