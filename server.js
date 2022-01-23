c1580a51f7dbb019643a704876fe95b060e8e1b3
Duan Pham <duanziggs@gmail.com>
2022-01-23 23:41:12 +0700

    update pakage.json

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(_dirname + './dist/delete-project'));
 
 app.get('/*', (req, res) =>
-    res.sendFile(path.join(_dirname,'dist','delete-project','index.html')),
+    res.sendFile(path.join(_dirname + '/dist/delete-project/index.html')),
 );
 
 // Start the app by listening on the default Heroku port
